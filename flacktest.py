39c72e85ed7d8652610dd346fde0c661bb55d51e
ZaidAG <gamerbrosimza@gmail.com>
2024-08-15 10:15:40 -0700

    Updates

--- flacktest.py
+++ flacktest.py
@@ -2,16 +2,18 @@ import requests
 import json
 from urllib.parse import urlparse
 
+def second_let(x):
+    return x[1]
 
 try:
     api_response = requests.get('https://jsonplaceholder.typicode.com/todos')
     api_respose_json = api_response.json()
-    print(api_respose_json)
-    for item in api_respose_json:
-        print(item)
     # Go through all of the todos and sort them by ascending alphabitcal order (a-z) based on the second letter in each todo's title
+    
+    
+    for item in api_respose_json:
+        print(second_let(item["title"]))
     
 except Exception as error:
     print("There was an error with the request", error)
-
 
